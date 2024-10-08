4c888cab2828849fc55eaa8d864436484d59dc20
ward033 <edwardchrisavila@gmail.com>
2024-10-08 17:01:41 +0800

    added navigation php file for quick editing of nav

--- servers.php
+++ servers.php
@@ -1,5 +1,5 @@
 <?php
-require_once "auth.php";
+include "navigation.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,78 +16,6 @@ require_once "auth.php";
     <script src="js/all.js" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
-<nav class="sb-topnav navbar navbar-expand navbar-light bg-light">
-    <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Masterfile</a>
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-    <!-- Navbar Search-->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-        <div class="input-group">
-
-        </div>
-    </form>
-    <!-- Navbar-->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Settings</a></li>
-                <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-            </ul>
-        </li>
-    </ul>
-</nav>
-<div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
-            <div class="sb-sidenav-menu">
-                <div class="nav">
-                    <div class="sb-sidenav-menu-heading">ANALYTICS</div>
-                    <a class="nav-link" href="index.php">
-                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                        Dashboard
-                    </a>
-                    <div class="sb-sidenav-menu-heading">ASSET</div>
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                        Hardwares
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="servers.php">Servers</a>
-                            <a class="nav-link" href="cpu-pc.php">CPU-PC</a>
-                            <a class="nav-link" href="#">Keyboard</a>
-                            <a class="nav-link" href="#">Monitor</a>
-                            <a class="nav-link" href="#">Mouse</a>
-                            <a class="nav-link" href="#">Printers</a>
-                        </nav>
-                    </div>
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                        Peripherals
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.html">Cash Drawer</a>
-                            <a class="nav-link" href="layout-static.html">LC Display</a>
-                            <a class="nav-link" href="layout-static.html">POS Machine</a>
-                            <a class="nav-link" href="layout-static.html">Sigpad</a>
-                            <a class="nav-link" href="layout-sidenav-light.html">Webcam</a>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-            <div class="sb-sidenav-footer">
-                <div class="small">Logged in as:</div>
-                Administrator
-            </div>
-        </nav>
-    </div>
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
@@ -325,15 +253,11 @@ require_once "auth.php";
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Add CPU-PC</button>
+                <button type="button" class="btn btn-primary">Add Server</button>
             </div>
         </div>
     </div>
 </div>
-
-<!--this is a comment-->
-<!-- this is my comment--->
-<!-- this is another comment-->
 <script src="js/jquery.main.js" type="text/javascript"></script>
 <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="js/scripts.js"></script>
