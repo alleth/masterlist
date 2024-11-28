ba7ffa28db1ad4def25b9aa8ac73ba670109ec82
ward033 <edwardchrisavila@gmail.com>
2024-11-28 15:17:50 +0800

    Dashboard update

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
                     <div class="row">
                         <div class="col-xl-3 col-md-6"><h3>This is a homepage.</h3></div>
                 </div>
-                <div class="container-fluid px-4">
+                <div class="container-fluid px-4 d-flex">
                         <div class="row">
-                            <div class="col-md-3 mb-4 mt-2">
+                            <div class="col-md-3 mb-4 mt-2 px-4">
                                 <div class="card card-body p-3" style="width: 18rem;">
                                     <p class="text-sm mb-0 text-capitalizatize font-weight-bold">Servers
                                         <h5 class="font-weight-bolder mb-0">
@@ -20,7 +20,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-3 mb-4">
+                            <div class="col-md-3 mb-4 mt-2 px-4">
                                 <div class="card card-body p-3" style="width: 18rem;">
                                     <p class="text-sm mb-0 text-capitalizatize font-weight-bold">CPU-PC
                                         <h5 class="font-weight-bolder mb-0">
@@ -32,7 +32,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-3 mb-4">
+                            <div class="col-md-3 mb-4 mt-2 px-4">
                                 <div class="card card-body p-3" style="width: 18rem;">
                                     <p class="text-sm mb-0 text-capitalizatize font-weight-bold">Printers
                                         <h3 class="font-weight-bolder mb-0">
