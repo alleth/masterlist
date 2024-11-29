e159bc0fa87b03adc57375f541341ead67aaa4db
ward033 <edwardchrisavila@gmail.com>
2024-11-29 17:28:48 +0800

    Another Update to index.php nad additional files

--- index.php
+++ index.php
@@ -64,6 +64,110 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        310
+                                    </h3>
+                                    <p class="fs-8">Router</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-router-fill fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        351
+                                    </h3>
+                                    <p class="fs-8">Switch</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-ethernet fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        528
+                                    </h3>
+                                    <p class="fs-8">Webcam</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-webcam-fill fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        498
+                                    </h3>
+                                    <p class="fs-8">Sigpad</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-clipboard-fill fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        3145
+                                    </h3>
+                                    <p class="fs-8">Keyboard</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-keyboard-fill fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        3122
+                                    </h3>
+                                    <p class="fs-8">Mouse</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-mouse2-fill fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        341
+                                    </h3>
+                                    <p class="fs-8">UPS-Server</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-battery-charging fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 border-2">
+                            <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                                <div>
+                                    <h3 class="fs-2">
+                                        3124
+                                    </h3>
+                                    <p class="fs-8">UPS-PC</p>
+                                </div>
+                                <div>
+                                    <i class="bi bi-battery-charging fs-1 primary-text rounded-full secondary-bg p-3"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
