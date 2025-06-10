2eaaf36f1d6a4b750dedba07fd2fa75fd3902cde
ward033 <edwardchrisavila@gmail.com>
2025-06-10 15:50:32 +0800

    new

--- DAO/hardware-add-details-DAO.php
+++ DAO/hardware-add-details-DAO.php
@@ -46,7 +46,6 @@ class addHardwareDAO extends BaseDAO {
                 $serialData = $serialDataStmt->fetch(PDO::FETCH_ASSOC);
                 echo "Serial Number Already Exist on site ";
                 echo "" . htmlspecialchars($serialData['site_code']) . "";
-                echo "" . htmlspecialchars($serialData['site_code']) . "";
             }
         //} 
         else {
